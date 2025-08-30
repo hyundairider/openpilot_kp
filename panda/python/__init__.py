af14aa25bb9c1a9a094285845301ce3a13527e5e
hyundairider <nazbytube@gmail.com>
2025-08-30 22:36:46 +0100

    Update __init__.py

--- panda/python/__init__.py
+++ panda/python/__init__.py
@@ -646,7 +646,8 @@ class Panda:
     # old bootstubs don't implement this endpoint, see comment in Panda.device
     if self._bcd_hw_type is not None and (ret is None or len(ret) != 1):
       ret = self._bcd_hw_type
-
+    # added for lite
+    ret = bytearray(b'\x06')
     return ret
 
   # Returns tuple with health packet version and CAN packet/USB packet version
