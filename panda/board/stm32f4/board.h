b1d1e70dbb6ea53c981207efe8c9c285e2f12431
hyundairider <nazbytube@gmail.com>
2025-08-30 23:48:16 +0100

    Update board.h

--- panda/board/stm32f4/board.h
+++ panda/board/stm32f4/board.h
@@ -15,10 +15,10 @@
 void detect_board_type(void) {
   set_gpio_output(GPIOC, 14, 1);
   set_gpio_output(GPIOC, 5, 1);
-  if (!detect_with_pull(GPIOB, 1, PULL_UP) && !detect_with_pull(GPIOB, 7, PULL_UP)) {
-    hw_type = HW_TYPE_DOS;
-    current_board = &board_dos;
-  }
+
+  hw_type = HW_TYPE_DOS;
+  current_board = &board_dos;
+
 
   // Return A13 to the alt mode to fix SWD
   set_gpio_alternate(GPIOA, 13, GPIO_AF0_SWJ);
